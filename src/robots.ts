cc04b2e9a06a9d37b5f594e415ee9231a0ff3e6a
Alejandro Alonso <alejandroalonso@Mac.lan>
2025-09-11 00:40:37 -0400

    update

--- src/robots.ts
+++ src/robots.ts
@@ -21,7 +21,5 @@ export default function robots(): MetadataRoute.Robots {
       ],
     },
     sitemap: 'https://paragoninvestments.org/sitemap.xml',
-    // crawlDelay is optional (Google ignores it, Bing respects it)
-    crawlDelay: 1,
   };
 }
