3c4a12ad380fd914d6437821e0e620623434187a
Alejandro Alonso <alejandroalonso@Mac.lan>
2025-09-11 00:40:37 -0400

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -87,5 +87,9 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ['/((?!_next|api|robots\\.txt|sitemap\\.xml|.*\\..*).*)'],
+  matcher: [
+    // Recommended by Clerk docs:
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
