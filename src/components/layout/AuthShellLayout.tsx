c04b482ca3f969bc24651e4523191560fbc86f56
anipotts <anirudhpottammal@nyu.edu>
2026-02-02 16:41:32 -0500

    small text edit

--- src/components/layout/AuthShellLayout.tsx
+++ src/components/layout/AuthShellLayout.tsx
@@ -14,7 +14,6 @@ export default function AuthShellLayout({
     <div className="flex min-h-screen bg-white relative">
       {/* Override body background so iOS safe-area inset shows white, not navy */}
       <style>{`html, body { background-color: #ffffff; }`}</style>
-
       {/* Mobile Top Bar */}
       <div className="lg:hidden fixed top-0 left-0 right-0 z-[60] bg-[#00172B] border-b border-[#003E6B] shadow-lg">
         <div className="flex items-center justify-between px-4 py-3">
@@ -33,7 +32,7 @@ export default function AuthShellLayout({
           </a>
         </div>
       </div>
-
+      ![1770060832761](image/AuthShellLayout/1770060832761.png)![1770060839398](image/AuthShellLayout/1770060839398.png)
       {/* Desktop Sidebar */}
       <aside className="hidden lg:flex flex-shrink-0 flex-col h-screen sticky top-0 w-[14rem] bg-[#00172B]">
         {/* Logo */}
@@ -62,7 +61,6 @@ export default function AuthShellLayout({
           </a>
         </div>
       </aside>
-
       {/* Content Area */}
       <div className="flex-1 min-w-0 bg-white">
         <div className="lg:px-12 lg:py-16 px-6 pt-24 pb-8 lg:pt-16">
